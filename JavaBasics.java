5fc4bd1a3cf5e89fe5552d80fd834f2e650d09ab
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-06-23 16:50:11 +0530

    Initial Commit

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Hello World!\n");
+        System.out.print("Hello World!");
+        
+    }
+}
+//boilerplate code.
