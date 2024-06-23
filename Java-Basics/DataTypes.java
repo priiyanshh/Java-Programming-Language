20ba53fca80b3b85e6631858a85d416352ce71fd
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-06-23 23:01:54 +0530

    Initial Commit

---
+++ Java-Basics/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 9;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = 1.5f;
+        System.out.println(price);
+
+        //long
+        //double
+
+    }
+}
---
+++ Java-Basics/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        //code to calculate sum.
+        int a = 45;
+        int b = 40;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
