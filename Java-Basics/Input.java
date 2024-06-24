a744be5aed90c3918b82df2f0f57d7f6b2c3c343
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-06-24 17:26:47 +0530

    Initial Commit

---
+++ Java-Basics/Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(input);
+
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
