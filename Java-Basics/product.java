1951fcc7c38a1007bdb2036914a00bcdaae5f9a8
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-06-25 22:00:03 +0530

    Initial Commit

---
+++ Java-Basics/product.java
@@ -0,0 +1,13 @@
+/*
+ Product of two number a and b and taking the input from the user!!
+ */
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a * b;
+        System.out.println(sum);
+    }
+}
