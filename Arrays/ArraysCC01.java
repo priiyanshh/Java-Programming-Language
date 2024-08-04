55f9a9c90bdcfa0a6eec58bbd20cc8e07c9fde1f
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-08-04 16:43:44 +0530

    first commt

---
+++ Arrays/ArraysCC01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArraysCC01 {
+    public static void main(String[] args) {
+        int marks[ ] = new int[50];
+
+        int numbers[ ] = {1, 2, 3}; //3
+
+        int moreNumbers[ ] = {4, 5, 6}; //3
+        
+
+    }
+}
