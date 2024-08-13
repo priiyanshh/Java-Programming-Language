9810636ce327175b96d9c07907a9183ea029dba7
Priyansh Gupta <priyanshgupta1227@gmail.com>
2024-08-13 22:56:50 +0530

    changes req

--- Stacks/stackC.java
+++ Stacks/stackC.java
@@ -47,6 +47,14 @@ public class stackC{
         }
     }
     public static void main(String[] args) {
-        
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
